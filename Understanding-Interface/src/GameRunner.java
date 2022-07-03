6968e2224bfb3d238300ec2b42ad2966dc0f9eec
EastZero123 <99165555+EastZero123@users.noreply.github.com>
2022-07-03 14:52:14 +0900

    추상메소드, 인터페이스 이해

--- Understanding-Interface/src/GameRunner.java
+++ Understanding-Interface/src/GameRunner.java
@@ -2,12 +2,21 @@
 public class GameRunner {
 
 	public static void main(String[] args) {
+		GamingConsole[] games = { new MarioGame(), new ChessGame() };
+
+		for (GamingConsole game : games) {
+			game.up();
+			game.down();
+			game.left();
+			game.right();
+		}
+
 		// GamingConsole game = new MarioGame();
-		GamingConsole game = new ChessGame();
-		game.up();
-		game.down();
-		game.left();
-		game.right();
+//		GamingConsole game = new ChessGame();
+//		game.up();
+//		game.down();
+//		game.left();
+//		game.right();
 
 	}
 
